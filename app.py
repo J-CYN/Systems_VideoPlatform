9a382c6296ad722b94fa8c72c39d8c7ae30cfaeb
Jamal Abudiab <jabudiab2024@gmail.com>
2025-11-08 22:27:05 -0500

    basic additions to app.py such as imports and dockerfile created

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, jsonify, render_template
 
+import os
+import re
+from datetime import datetime
+from azure.storage.blob import BlobServiceClient
+
+app = Flask(__name__)
+
+@app.get("/")
+def homepage():
+    return render_template("index.html")
