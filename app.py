12289b5a51668be84fdada1f3576e7a65d764257
Jamal Abudiab <jabudiab2024@gmail.com>
2025-11-12 14:24:43 -0500

    App.py upload function is set up a bit more, and the start of a new function of api/watch was created in comments

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
-CONTAINER_NAME=
+CONTAINER_NAME="video_catalog"
 
 bsc = BlobServiceClient.from_connection_string(os.getenv("AZURE_STORAGE_CONNECTION_STRING"))
 cc  = bsc.get_container_client(CONTAINER_NAME)
@@ -20,7 +20,15 @@ def homepage():
 def upload():
     if "file" not in request.files:
         return jsonify(ok=False, error="No file part in request"), 400
+    
+    f = request.files["file"]
+    if f.filename == "":
+        return jsonify(ok=False, error="No selected file"), 40
     #Continue Code here
+    try:
+        
+    except Exception as e:
+        return jsonify(ok=False, error=str(e)), 500
 
 @app.get("/api/catalog")
 def catalog():    
@@ -29,6 +37,12 @@ def catalog():
 @app.get("/api/health")
 def health():
     return jsonify(status="ok"), 200
+
+"""
+@app.get("/api/watch")
+def catalog():    
+    #Continue Code here
+"""
 
 #if __name__ == "__main__":
 #    app.run(host="0.0.0.0", port=8080)
